70e26ac8b75b63aa22c54de79d8e94bf82799fdd
DanilMir <mirgayazov02@gmail.com>
2022-06-06 17:11:24 +0300

    working counter

---
+++ CreditApp/ClientApp/src/components/Counter.tsx
@@ -0,0 +1,20 @@
+import React, {Component, useState} from 'react';
+
+export const Counter = () => {
+  // static displayName = Counter.name;
+    const [currentCount, incrementCounter] = useState(0);
+  
+    return (
+      <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a React component.</p>
+
+        <p aria-live="polite">Current count: <strong>{currentCount}</strong></p>
+
+        <button className="btn btn-primary" onClick={() => {
+            incrementCounter(currentCount + 1)
+        }}>Increment</button>
+      </div>
+    );
+}
