d37db745c36dca369f6e4398b5aabaeebfbca4af
DanilMir <mirgayazov02@gmail.com>
2022-06-06 17:30:29 +0300

    working layout, navMenu

--- CreditApp/ClientApp/src/components/Layout.tsx
+++ CreditApp/ClientApp/src/components/Layout.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render () {
+export const Layout = (props : any) => {
+  // static displayName = Layout.name;
+    
     return (
       <div>
         <NavMenu />
         <Container>
-          {this.props.children}
+          {props.children}
         </Container>
       </div>
     );
-  }
 }
